1a97516999454cc5a072eb9244e62d0dac67b3a8
Winter Cyan <wintercyan@Winters-MacBook-Pro.local>
2019-10-06 17:44:53 +0800

    deferred rendering finished.

--- OpenGL/defer.cpp
+++ OpenGL/defer.cpp
@@ -8,8 +8,6 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -18,6 +16,7 @@
 
 #include "MyShader.h"
 #include "MyCamera.h"
+#include "Model.h"
 
 #define WIDTH 1200
 #define HEIGHT 800
@@ -69,13 +68,92 @@ int main() {
     glEnable(GL_DEPTH_TEST);
     
     // shaders
-    MyShader sceneShader("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/vs.bloom.scene", "/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/fs.bloom.scene", NULL);
+    MyShader fboShader("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/vs.defer", "/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/fs.defer", NULL);
+    fboShader.use();
+    fboShader.setInt("color_texture", 0);
+    fboShader.setInt("specular_texture", 1);
+    MyShader quadShader("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/vs.defer.quad", "/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/fs.defer.quad", NULL);
+    quadShader.use();
+    quadShader.setInt("allColor", 0);
+    MyShader sceneShader("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/vs.defer.scene", "/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/fs.defer.scene", NULL);
+    sceneShader.use();
+    sceneShader.setInt("position_texture", 0);
+    sceneShader.setInt("normal_texture", 1);
+    sceneShader.setInt("albedo_texture", 2);
+    MyShader lightShader("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/vs.defer.light", "/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/fs.defer.light", NULL);
     
-    // textures
-    unsigned int boxTexture = loadTexture("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/container2.png", true);
+    // textures & models
+    Model nanosuit("/Users/wintercyan/XCODE/OpenGL/LearnOpenGL/nanosuit/nanosuit.obj");
     
     // create FBO
-
+    unsigned int FBO;
+    glGenFramebuffers(1, &FBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+    // create 3 textures
+    unsigned int positionTexture, normalTexture, albedoTexture;
+    // position texture
+    glGenTextures(1, &positionTexture);
+    glBindTexture(GL_TEXTURE_2D, positionTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, WIDTH, HEIGHT, 0, GL_RGB, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, positionTexture, 0);
+    // normal texture
+    glGenTextures(1, &normalTexture);
+    glBindTexture(GL_TEXTURE_2D, normalTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, WIDTH, HEIGHT, 0, GL_RGB, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, normalTexture, 0);
+    // color & specular texture
+    glGenTextures(1, &albedoTexture);
+    glBindTexture(GL_TEXTURE_2D, albedoTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WIDTH, HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, albedoTexture, 0);
+    // tell opengl to use 3 color attachments
+    unsigned int attachments[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
+    glDrawBuffers(3, attachments);
+    // create render buffer
+    unsigned int RBO;
+    glGenRenderbuffers(1, &RBO);
+    glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, WIDTH, HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, RBO);
+    // finally check if framebuffer is complete
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer not complete!" << std::endl;
+    
+    std::vector<glm::vec3> objectPositions;
+    objectPositions.push_back(glm::vec3(-3.0,  -3.0, -3.0));
+    objectPositions.push_back(glm::vec3( 0.0,  -3.0, -3.0));
+    objectPositions.push_back(glm::vec3( 3.0,  -3.0, -3.0));
+    objectPositions.push_back(glm::vec3(-3.0,  -3.0,  0.0));
+    objectPositions.push_back(glm::vec3( 0.0,  -3.0,  0.0));
+    objectPositions.push_back(glm::vec3( 3.0,  -3.0,  0.0));
+    objectPositions.push_back(glm::vec3(-3.0,  -3.0,  3.0));
+    objectPositions.push_back(glm::vec3( 0.0,  -3.0,  3.0));
+    objectPositions.push_back(glm::vec3( 3.0,  -3.0,  3.0));
+    
+    const unsigned int LIGHT_NUM = 32;
+    std::vector<glm::vec3> lightPositions;
+    std::vector<glm::vec3> lightColors;
+    srand(13);
+    for (unsigned int i = 0; i < LIGHT_NUM; i++)
+    {
+        // calculate slightly random offsets
+        float xPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+        float yPos = ((rand() % 100) / 100.0) * 6.0 - 4.0;
+        float zPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+        lightPositions.push_back(glm::vec3(xPos, yPos, zPos));
+        // also calculate random color
+        float rColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+        float gColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+        float bColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+        lightColors.push_back(glm::vec3(rColor, gColor, bColor));
+    }
+    
     while (!glfwWindowShouldClose(window)) {
         float currFrame = glfwGetTime();
         deltaTime = currFrame - lastFrame;
@@ -90,6 +168,56 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
 //         render to FBO
+        glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        fboShader.use();
+        fboShader.setMat4("projection", projection);
+        fboShader.setMat4("view", view);
+        for (unsigned int i = 0; i < objectPositions.size(); i++)
+        {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, objectPositions[i]);
+            model = glm::scale(model, glm::vec3(0.25f));
+            fboShader.setMat4("model", model);
+            nanosuit.Draw(fboShader);
+        }
+        
+        // render scene
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        sceneShader.use();
+        sceneShader.setVec3("viewPos", camera.Position);
+        for (unsigned int i = 0; i < LIGHT_NUM; i ++) {
+            sceneShader.setVec3("lights[" + to_string(i) + "].Pos", lightPositions[i]);
+            sceneShader.setVec3("lights[" + to_string(i) + "].Color", lightColors[i]);
+            sceneShader.setFloat("lights[" + to_string(i) + "].Linear", 0.7f);
+            sceneShader.setFloat("lights[" + to_string(i) + "].Quadratic", 1.8f);
+        }
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, positionTexture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, normalTexture);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, albedoTexture);
+        renderQuad();
+        
+        // render light cube
+        glViewport(0, 0, WIDTH, HEIGHT);
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, FBO);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        glBlitFramebuffer(0, 0, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        lightShader.use();
+        lightShader.setMat4("projection", projection);
+        lightShader.setMat4("view", view);
+        for (unsigned int i = 0; i < lightPositions.size(); i ++) {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, lightPositions[i]);
+            model = glm::scale(model, glm::vec3(.125f));
+            lightShader.setMat4("model", model);
+            lightShader.setVec3("lightColor", lightColors[i]);
+            renderCube();
+        }
         
         glfwSwapBuffers(window);
         glfwPollEvents();
