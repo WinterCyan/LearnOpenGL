76341fa4b267497514a97c3bfb03b5415f497a00
wintercyan <wintercyan98@gmail.com>
2020-07-18 15:52:38 +0800

    done.

---
+++ OpenGL/cmake-build-debug/CMakeFiles/IBL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IBL.dir/IBLCopy.cpp.o"
+  "IBL"
+  "IBL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IBL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
