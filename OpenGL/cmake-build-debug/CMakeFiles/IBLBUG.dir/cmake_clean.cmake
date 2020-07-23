86641a2cca37011ec2e0b9854d2731431e519f11
wintercyan <wintercyan98@gmail.com>
2020-07-23 15:22:30 +0800

    "adding model-loading..."

---
+++ OpenGL/cmake-build-debug/CMakeFiles/IBLBUG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IBLBUG.dir/IBL.cpp.o"
+  "IBLBUG"
+  "IBLBUG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IBLBUG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
